d6947a9e414f541bdb0492f5ffebbe962b4c988f
difalcomarco <marco.difalco@dedalus.eu>
2019-10-28 12:07:34 +0100

    primo commit
    primo commit

---
+++ build/precache-manifest.c91cb8630bb5bb8f4b52a192e76432f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c763a8ff1b570bac1c070214840f60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18cb98d5b6f6b4831da2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "158aad06e3f749abf604",
+    "url": "/static/js/2.741bb5c4.chunk.js"
+  },
+  {
+    "revision": "18cb98d5b6f6b4831da2",
+    "url": "/static/js/main.cf14b067.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
