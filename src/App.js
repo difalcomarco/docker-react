b4aed51f9bfd96999bf0cbbc594f4b607c1d6e16
Marco Di Falco <marco.difalco@dedalus.eu>
2019-10-28 10:59:17 +0100

    primo commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Le tette di Ilaria che si sognano tutti quanti soprattutto CHRISTIAN!!!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
